979673c4556f7598003a3e74cbe32db6c3d9c5ba
Showkat  Ali <showkat.ali2634@gmail.com>
2025-05-21 19:52:12 +0600

    addng

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,10 @@ const app: Application = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ 
+  origin: ['http://localhost:3000', 'https://think-trail.vercel.app'],
+  credentials: true 
+}));
 
 // application routes
 app.use('/api/v1', router);
