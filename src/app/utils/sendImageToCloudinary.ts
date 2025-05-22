76d394b76314a1d65a4e6d08dd7a06ecdc2e9ba3
Showkat  Ali <showkat.ali2634@gmail.com>
2025-05-22 20:03:15 +0600

    adding

--- src/app/utils/sendImageToCloudinary.ts
+++ src/app/utils/sendImageToCloudinary.ts
@@ -1,7 +1,8 @@
 import { UploadApiResponse, v2 as cloudinary } from 'cloudinary';
 import multer from 'multer';
 import config from '../config';
-
+import fs from 'fs';
+import path from 'path';
 cloudinary.config({
   cloud_name: config.cloudinary_cloud_name,
   api_key: config.cloudinary_api_key,
@@ -38,7 +39,11 @@ export const sendImageToCloudinary = (
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, process.cwd() + '/uploads/');
+    const uploadPath = path.join(process.cwd(), '/uploads/');
+    if (!fs.existsSync(uploadPath)) {
+      fs.mkdirSync(uploadPath, { recursive: true }); // Create the directory if it doesn't exist
+    }
+    cb(null, uploadPath);
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
